a3eafb42a665dd1167f5791a4ee6914964288331
Abhishek <88420201+abhiijndal@users.noreply.github.com>
2021-09-15 10:17:55 +0530

    Add files via upload

---
+++ face_detection.py
@@ -0,0 +1,19 @@
+import cv2 as cv
+img= cv.imread(r"C:\Users\91821\OneDrive\Pictures/img.jpg")
+
+resize=cv.resize(img,(500,500),interpolation=cv.INTER_CUBIC)
+#cv.imshow('img',resize)
+
+gray=cv.cvtColor(resize,cv.COLOR_BGR2GRAY)
+#cv.imshow('gray',gray)
+
+haar_cas=cv.CascadeClassifier('haar_cas.xml')
+face_react=haar_cas.detectMultiScale(gray,scalefactor=1.1,minNeighbors=3)
+print(f'number of face detected:{len(face_react)}')
+for (x,y,w,h) in face_react:
+
+    cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),thickness=2)
+    
+cv.imshow('detected_face',img)
+cv.waitKey(0)
+
