f9f42d2ac1d2d08acaeaa505148e19e0b1ed8dd7
Abhishek <88420201+abhiijndal@users.noreply.github.com>
2021-09-15 13:38:37 +0530

    Update face_detection.py

--- face_detection.py
+++ face_detection.py
@@ -1,14 +1,14 @@
 import cv2 as cv
-img= cv.imread(r"C:\Users\91821\OneDrive\Pictures/img.jpg")
+img= cv.imread(r"C:\Users\91821\OneDrive\Pictures/group.jpg")
 
-resize=cv.resize(img,(500,500),interpolation=cv.INTER_CUBIC)
+#resize=cv.resize(img,(500,500),interpolation=cv.INTER_CUBIC)
 #cv.imshow('img',resize)
 
 gray=cv.cvtColor(resize,cv.COLOR_BGR2GRAY)
 #cv.imshow('gray',gray)
 
 haar_cas=cv.CascadeClassifier('haar_cas.xml')
-face_react=haar_cas.detectMultiScale(gray,scalefactor=1.1,minNeighbors=3)
+face_react=haar_cas.detectMultiScale(gray,minNeighbors=3)
 print(f'number of face detected:{len(face_react)}')
 for (x,y,w,h) in face_react:
 
